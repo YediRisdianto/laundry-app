f7390269f5996f312ee088cefabfe3868a8929cc
YediRisdianto <yedi.risdianto1@gmail.com>
2025-06-02 16:18:38 +0700

    add : page manage customer and service

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ServiceController;
 
 // Route::get('/', function () {
 //     return view('auth.login');
@@ -12,3 +14,5 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::Class, 'authenticate'])->name('login.auth');
 
 Route::get('/dashboard', [DashboardController::class, 'index' ])->name('dashboard');
+Route::get('/master/customer', [CustomerController::class, 'index'])->name('master.customer');
+Route::get('/master/service', [ServiceController::class, 'index'])->name('master.service');
